b3b1ff7364295061c507cbff02eb10895a23ae16
Havanur <havanurtosun0618@gmail.com>
2021-04-23 16:58:02 -0400

    Python Assignment

---
+++ assigment.py
@@ -0,0 +1,10 @@
+answer = 'Havanur'
+question = 'What is your name?  '
+print ("Let's find the password!")
+while True:
+    guess = str(input(question))
+   if guess == answer:
+      print("Hello, Havanur! The password is : W@12")
+   else:
+      print("Hello, Amina! See you later.")
+   break
